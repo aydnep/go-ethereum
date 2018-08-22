2106609a219427025f814598d01fbb370c0e9411
Andrii Pindiura <aydnep@aydnep.com.ua>
2018-08-22 20:43:19 +0300

    Update worker.go

--- miner/worker.go
+++ miner/worker.go
@@ -786,7 +786,7 @@ func (w *worker) commitNewWork(interrupt *int32, noempty bool) {
 		tstamp = parent.Time().Int64() + 1
 	}
 	// this will ensure we're not going off too far in the future
-	if now := time.Now().Unix(); tstamp > now {
+	if now := time.Now().Unix(); tstamp > now+1 {
 		wait := time.Duration(tstamp-now) * time.Second
 		log.Info("Mining too far in the future", "wait", common.PrettyDuration(wait))
 		time.Sleep(wait)
